795683fecd24bc4e0c0a1176ccb0c7f0bc6d83fd
Matias Eyzaguirre <matias@fortlogic.net>
2014-11-29 21:24:24 -0700

    Added delimiters, dy/dx fractions, and mathbf.

--- math.rkt
+++ math.rkt
@@ -1,29 +1,51 @@
 #lang at-exp racket
 
-(provide cal mcal bb mbb sf msf rm mrm)
+(provide cal mcal bb mbb bf mbf sf msf rm mrm dd delim)
 
 (require "utils.rkt")
 
+(define (value->content v)
+  (cond
+    [(string? v) (list "{" v "}")]
+    [(char? v) (list->string (list v))]
+    [(number? v) (list "{" (number->string v) "}")]))
+
 (define (cal . stuff)
-  (list "\\mathcal{" stuff "}"))
+  (list "{\\mathcal{" stuff "}}"))
 
 (define (mcal . stuff)
-  (m "\\mathcal{" stuff "}"))
+  (m (cal stuff)))
 
 (define (bb . stuff)
-  (list "\\mathbb{" stuff "}"))
+  (list "{\\mathbb{" stuff "}}"))
 
 (define (mbb . stuff)
-  (m "\\mathbb{" stuff "}"))
+  (m (bb stuff)))
+
+(define (bf . stuff)
+  (list "{\\mathbf{" stuff "}}"))
+
+(define (mbf . stuff)
+  (m (bf stuff)))
 
 (define (sf . stuff)
-  (list "\\mathsf{" stuff "}"))
+  (list "{\\mathsf{" stuff "}}"))
 
 (define (msf . stuff)
-  (m "\\mathsf{" stuff "}"))
+  (m (sf stuff)))
 
 (define (rm . stuff)
-  (list "\\mathrm{" stuff "}"))
+  (list "{\\mathrm{" stuff "}}"))
 
 (define (mrm . stuff)
-  (m "\\mathrm{" stuff "}"))
+  (m (rm stuff)))
+
+(define (dd var . stuff)
+  (list "{\\frac{d" stuff "}{d" var "}}"))
+
+(define (delim delims . stuff)
+  (list "\\left"
+        (value->content (sequence-ref delims 0))
+        stuff
+        "\\right"
+        (value->content (sequence-ref delims 1))))
