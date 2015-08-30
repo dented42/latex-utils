cc2ce27cc4700c60088f1696de025dfadea59d16
Matias Eyzaguirre <matias@fortlogic.net>
2015-08-29 22:43:46 -0600

    Added notes for a figure module I want to make, so far does nothing.

---
+++ figure.rkt
@@ -0,0 +1,6 @@
+#lang racket/base
+
+;;; Takes some content, and wraps it in something that makes the problem in
+;;; http://tex.stackexchange.com/questions not an issue.
+(define (wide . content)
+  #f)
